44ec6fde6729f8b2408833ac4435cdea8a362338
Andrii Yerzhykevych <andriiyerzh@gmail.com>
2026-03-03 00:58:09 +0200

    chore: test

--- src/middleware/approvedGuest.js
+++
@@ -1,5 +0,0 @@
-module.exports = async function approvedGuest(request, reply) {
-  if (request.user.approvalStatus !== 'approved') {
-    return reply.code(403).send({ message: 'Your request to join is pending admin approval' })
-  }
-}
